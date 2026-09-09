764e767c794eab3641084d3bfc6de6c05ef056f0
Bonaeineza31 <b.ineza@alustudent.com>
2026-09-09 21:25:03 +0200

    docs: add comments to mycomponent tsx and css

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './MyComponent.css';
 
+// Define the MyComponent functional component
 const MyComponent: React.FC = () => {
   return (
+    // JSX structure for the component
     <div className="my-component">
       <h1>Hello from MyComponent!</h1>
       <p>This is a paragraph of text within my component.</p>
